cda49061eba0901debf8546b87d00248843c5fa5
Mrrabie1991 <mrrabie1991@gmail.com>
2025-09-02 00:23:37 +0330

    test

--- prac2/main.cpp
+++ prac2/main.cpp
@@ -13,23 +13,83 @@ class Student{
 };
 
 vector <Student> students;
-map<string,int> studentMap;
+map<int,string> studentMap;
+int app = 1;
+
 void mapIng(vector <Student> students){
 	for(auto &s : students){
-		studentMap[s.name]=s.id;
+		studentMap[s.id]=s.name;
 	}
 }
-int main(){
 
+void serchStudent (){
+	system("clear");
+	int i;
+	try{
+		cin >> i;
+		try{
+			if(studentMap.find(i) != studentMap.end()){
+				cout << "Name : " <<studentMap.find(i) <<" ID " << studentMap.find(i) <<" .\n";}
+		}catch(...){
+		
+		}
+
+	}catch(...){
+		cout <<"Enter the correct input.\n";	
+	}
+}
+
+
+void exit(){
+	system("clear");
+	app = 0;
+}
+void showStudent(){}
+void addStudent(){}
+int main(){;
+	int numOption;
 	students.push_back(Student("Sara",101));
 	students.push_back(Student("Amir",102));
 	students.push_back(Student("Omid",103));
 	
 	mapIng(students);
-	while(1){
+	while(app){
+	numOption = 0;
+	system("clear");
+	cout <<"Please select the appropriate option.\n";
+	cout <<"1 # List of all students.\n";
+	cout <<"2 # Search for a student by ID.\n";
+	cout <<"3 # Add a new student.\n";
+	cout <<"4 # Exit. \n";
+
+	try{
+		cin >> numOption;
+		throw(99);
+		if(numOption < 5 && numOption > 0){
+			switch(numOption){
+				case 1:
+					showStudent();
+					break;
+				case 2:
+					serchStudent();
+					break;
+				case 3:
+					addStudent();
+					break;
+				case 4:
+					exit();
+					break;
 		
-	for(auto &s : studentMap){
-		cout <<"Name : "<<s.first<<" ID : "<<s.second<<endl;
+			}
+		}else{
+			cout <<"Enter the correct input.\n";  
+			return 0;
+		}
 	}
+	catch(...){
+		cout <<"Enter the correct input.\n";
+		return 0;
+	}
+
 	}
 }
